28f957b09bc55076c3c926df34dcc8b38bbc0d29
Yash Paneliya <yashsp1158@gmail.com>
2021-12-30 13:49:35 +0530

    @fontawesome dependency added

---
+++ src/static/icons/awesomeIcons.js
@@ -0,0 +1,24 @@
+import styled, { useTheme } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// const BalanceIcon = <FontAwesomeIcon icon={faBalanceScale} />;
+// const SavingIcon = <FontAwesomeIcon icon={faPiggyBank} size="5x" />;
+// const GraphIcon = <FontAwesomeIcon icon={faChartLine} />;
+// const UmbrellaIcon = <FontAwesomeIcon icon={faUmbrella} />;
+// const CompanyIcon = <FontAwesomeIcon icon={faBuilding} />;
+// const BankIcon = <FontAwesomeIcon icon={faUniversity} />;
+
+const IconWrapper = styled.div``;
+
+const SIPIcon = (props) => {
+  const theme = useTheme();
+
+  console.log(props.icon);
+  return (
+    <IconWrapper>
+      <FontAwesomeIcon icon={props.icon} size="5x" color={theme.color} />
+    </IconWrapper>
+  );
+};
+
+export default SIPIcon;
